01add0b84a68dd448773ea35b60ec843010d218a
alpaqers <pawko0202@gmail.com>
2026-05-28 17:49:52 +0200

    reformat: black reformat

--- utils.py
+++ utils.py
@@ -5,14 +5,17 @@ def add(a: int, b: int) -> int:
     """Return sum of two numbers"""
     return a + b
 
-def subtract (a: int , b: int) -> int :
+
+def subtract(a: int, b: int) -> int:
     """Return difference of two numbers"""
     return a - b
 
-def multiply (a: int , b: int) -> int :
+
+def multiply(a: int, b: int) -> int:
     """Return product of two numbers"""
     return a * b
 
-def divide ( a: int , b : int ) -> float :
+
+def divide(a: int, b: int) -> float:
     """Return quotient of two numbers"""
     return a / b
